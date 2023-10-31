a4dc2dfdd233a490bb001070ff0385a99143dea8
Mian Asif Langrah <143902006+Asif-langrah@users.noreply.github.com>
2023-10-31 21:54:49 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+//store name in varioable and print message 
+let personName = "sohail";
+let message = `Hellow ${personName}, hope you are doing well`;
+console.log(message);
---
+++ index.ts
@@ -0,0 +1,5 @@
+//store name in varioable and print message 
+
+let personName:string="sohail"
+let message:string=`Hellow ${personName}, hope you are doing well`
+console.log(message);
